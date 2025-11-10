9753da67d3d78a1446b3f1cec87befe3e6c28741
User <user@example.com>
2025-11-10 21:26:59 +0800

    优化代码结构

--- backend/chatApp/main.go
+++ backend/chatApp/main.go
@@ -9,25 +9,6 @@ import (
 	"log"
 )
 
-// func main1() {
-// 	ctx := context.Background()
-
-// 	//使用message模版
-// 	fmt.Printf("===create messages===\n")
-// 	message := chat.MessagesTemplate()
-// 	//fmt.Printf("messages: %+v\n\n", chat.MessagesTemplate())
-
-// 	//创建llm
-// 	fmt.Printf("===create llm===\n")
-// 	model := chat.CreatOpenAiChatModel(ctx)
-// 	//log.Printf("create llm success\n\n")
-
-// 	//使用llm生成Steam流式回复
-// 	fmt.Printf("===llm stream ===\n")
-// 	streamResult := chat.Stream(ctx, model, message)
-// 	chat.ReportSteam(streamResult)
-// }
-
 func main() {
 	ctx := context.Background()
 	runner := agent.NewInterviewProcessAgent()
